3ba9b5c3f864c839e48472658912733d6e909b5e
smutnyjoe <marbit12@gmail.com>
2017-02-04 21:49:41 +0100

    added test_all.sh script

--- HTTAnalysis/test_all.sh
+++ HTTAnalysis/test_all.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-#This script runs test equal to "./test htt.ini" N times, each time for 1, 2, 3...10 threads.
+#This script runs test equal to "./test htt.ini" N times, each time for 1, 2, 3...T threads.
 #Creates if necessary directory ./tests, and subdirectories for each number of threads with subdirectories for each test.
 #Saves output of each test and resulting png_jpg images to proper directories.
+# USAGE: bash test_all.sh N T 
 
-N_TESTS=0 #number of tests for each number of threads passed by user
-MAX_THREADS=10
+N_TESTS=1 #number of tests for each number of threads passed by user
+T_THREADS=10 #maximal number of threads to use during each test
 
 usage() {
-	echo "Usage: bash ./test_all N"
-	echo "(Run N times: ./test htt.ini using 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 threads, save output and png_jpg to ./tests/"
+	echo "Usage: bash ./test_all N T"
+	echo "(Run N times ''./test htt.ini'', each time for 1,2...T threads, save output and png_jpg to ./tests/T/N"
 }
 
 parse_argument() {
-	if [[ $# -ne 1 ]]; then
+	if [[ $# -ne 2 ]]; then
 		echo "Illegal number of parameters"
 		usage
 		exit 1
@@ -24,13 +25,13 @@ parse_argument() {
 
 create_directories() {
 	mkdir -p tests #create tests directory if not exist
-	for (( i=1; $i <= $MAX_THREADS; i++ )); do
+	for (( i=1; $i <= $T_THREADS; i++ )); do
 		mkdir -p tests/$i #create tests/thread_no directory if not exist
 		cp htt.ini tests/$i/ #copy main htt.ini to this directory
 		echo "%s/threads = 1/threads = $i/g
 		w
 		q
-		" | ex tests/$i/htt.ini #change "threads = 1" to "threads = thread_no"
+		" | ex tests/$i/htt.ini #change "threads = 1" to "threads = i"
 		for (( j=1; $j <= $N_TESTS; j++ )); do
 			mkdir -p tests/$i/$j #create subdirectories for each of N_TESTS tests
 		done
@@ -38,8 +39,8 @@ create_directories() {
 }
 
 run_tests() {
-	for (( i=1; $i <= $MAX_THREADS; i++ )); do
-			htt_ini_path="tests/$i/htt.ini" #path to htt.ini containing "threads = N_THREADS"
+	for (( i=1; $i <= $T_THREADS; i++ )); do
+			htt_ini_path="tests/$i/htt.ini" #path to htt.ini containing "threads = i"
 		for (( j=1; $j <= $N_TESTS; j++ )); do
 			echo -n "./test $htt_ini_path"
 			echo ""
